4e601b583d5503e14578300a72d9556e7804ab78
Miz ha <hk.haru0803@gmail.com>
2022-11-13 22:25:19 +0900

    プロジェクト ファイルを追加します。

---
+++ GUI_Osero/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Osero
+{
+    public partial class Form1 : Form
+    {
+        //picturBoxコントロールの配列フィールド作成
+        private System.Windows.Forms.PictureBox[,] stone;
+        Stone stone_status = new Stone();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //formのロードイベントハンドラ
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //駒のステータス初期化
+            stone_status.def_set();
+
+            //マスの配列作成
+            this.stone = new System.Windows.Forms.PictureBox[8,8];
+
+            //picturBoxのインスタンス作成
+            for(Int16 i = 0; i < 8; i++)
+            {
+                for(Int16 j = 0; j < 8; j++)
+                {
+                    this.stone[i,j] = new System.Windows.Forms.PictureBox();
+                    //プロパティ設定
+                    this.stone[i, j].Name = "Stone" + i.ToString() + j.ToString();
+                    this.stone[i, j].Size = new Size(50, 50);
+                    this.stone[i, j].Location = new Point(12 + (50 * i), 12 + (50 * j));
+                    this.stone[i, j].SizeMode = PictureBoxSizeMode.Zoom;
+                    this.stone[i, j].Tag = "P" + i.ToString() + j.ToString();
+
+                    if (stone_status.stones[i,j] == 2)
+                    {
+                        this.stone[i, j].Image = System.Drawing.Image.FromFile(@"..\..\Properties\None.png");
+                    }
+                    else if(stone_status.stones[i,j] == 0)
+                    {
+                        this.stone[i, j].Image = System.Drawing.Image.FromFile(@"..\..\Properties\White.png");
+                    }
+                    else
+                    {
+                        this.stone[i, j].Image = System.Drawing.Image.FromFile(@"..\..\Properties\Black.png");
+                    }
+
+                    this.Controls.Add(this.stone[i, j]);
+
+                    this.stone[i,j].Click += new System.EventHandler(this.stone_Click);
+                }
+            }
+            this.ResumeLayout(false);
+        }
+
+        
+        private void stone_Click(object sender ,EventArgs e)
+        {
+            //クリックされたオブジェクトからTagを取得し、座標として変換
+            Int16 i = Int16.Parse(((PictureBox)sender).Tag.ToString().Substring(1, 1));
+            Int16 j = Int16.Parse(((PictureBox)sender).Tag.ToString().Substring(2, 1));
+
+            //取得した座標からデータ上ひっくり返す
+            stone_status.put(i, j);
+
+            //全データを参照してマスの表示切替
+            for (Int16 x = 0; x < 8; x++)
+            {
+                for (Int16 y = 0; y < 8; y++)
+                {
+                    if (stone_status.stones[x, y] == 0)
+                    {
+                        this.stone[x, y].Image = System.Drawing.Image.FromFile(@"..\..\Properties\White.png");
+                    }
+                    else if (stone_status.stones[x, y] == 1)
+                    {
+                        this.stone[x, y].Image = System.Drawing.Image.FromFile(@"..\..\Properties\Black.png");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ GUI_Osero/Stone.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace GUI_Osero
+{
+    internal class Stone
+    {
+        //各座標のマスのデータ
+        public Int16[,] stones = new Int16[8, 8];
+
+        /*ターンを決める変数
+         0：白のターン
+         1：黒のターン*/
+        public Int16 turn = 1;
+
+        //盤面初期化
+        public void def_set()
+        {
+            for(Int16 i = 0; i < 8; i++)
+            {
+                for(Int16 j = 0; j < 8; j++)
+                {
+                    stones[i, j] = 2;
+                }
+            }
+            stones[3, 3] = 0;
+            stones[4, 4] = 0;
+            stones[3, 4] = 1;
+            stones[4, 3] = 1;
+        }
+        //データ上で配置とひっくり返しのメソッド
+        public void put(Int16 i, Int16 j)
+        {
+            //配置可能かどうかを格納する
+            bool putable = false;
+
+            if (this.stones[i, j] == 2)  //駒配置の前提として配置場所が空白（2）である必要がある
+            {
+                /*置きたいマス（0，0）を基準に方向を指定する（x,y）
+                  ____________________
+                 |-1，-1| 0, -1| 1, -1|
+                 |______|______|______|
+                 |-1,  0| 0,  0| 1,  0|
+                 |______|______|______|
+                 |-1,  1| 0,  1| 1,  1|
+                 |______|______|______|
+                
+                 順に方向を指定して探索する*/
+                for(Int16 x = -1; x <= (Int16)1; x+=(Int16)1)
+                {
+                    for(Int16 y = -1; y <= (Int16)1; y+=(Int16)1)
+                    {
+                        //（0，0）は除外
+                        if (x == 0 && y == 0) { }
+                        else
+                        {
+                            //探索方向の2マス先から自分の駒がないか探索
+                            for (Int16 t = 2; t < 8; t++)
+                            {
+                                //探索中のマスが配列の定義内の時かつ探索方向の隣が相手の駒のとき
+                                if (i + (t * x) < (Int16)8 && i + (t * x) >= (Int16)0 && j + (t * y) < (Int16)8 && j + (t * y) >= (Int16)0 && this.stones[i + x, j + y] == (Int16)((this.turn + 1) % 2))
+                                {
+                                    //探索先のマスが空白（2）だった時はひっくり返せないので即ブレイク
+                                    if (this.stones[i + (t * x), j + (t * y)] == 2)
+                                    {
+                                        break;
+                                    }
+                                    //探索先が自分の駒だった場合
+                                    else if (this.stones[i + (t * x), j + (t * y)] == this.turn)
+                                    {
+                                        for (Int16 k = 0; k < t; k++)
+                                        {
+                                            //置きたいマスと自分の駒の間を自分の駒にする
+                                            this.stones[i + (x * k), j + (y * k)] = this.turn;
+                                        }
+                                        //配置可能であり、配置が完了したので真とする
+                                        putable = true;
+                                        //この方向の探索は終了とし、ブレイクする
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            if(putable == true)
+            {
+                //配置完了したのでターンを次に進める
+                this.turn = (Int16)((this.turn + 1) % 2); 
+            }
+        }
+
+
+    }
+}
